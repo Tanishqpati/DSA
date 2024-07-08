c6b4d43a851772d38d7ee1ea1d8279904cf4cbe6
Tanishq Patidar <tanishq.patidar@kalvium.community>
2024-07-08 09:24:18 +0530

    Delete kth element

--- LinkedList/DoublyLinkedList.cpp
+++ LinkedList/DoublyLinkedList.cpp
@@ -39,9 +39,14 @@ void print(Node* head){
 }
 
 Node* deleteHead(Node* head){
-    head = head->next;
-    head->prev=nullptr;
-    return head;
+    if (head == nullptr) return nullptr;
+
+    Node* newHead = head->next;
+    if (newHead != nullptr) {
+        newHead->prev = nullptr;
+    }
+    delete head;
+    return newHead;
 }
 void deleteTail(Node* head){
     Node* temp = head;
@@ -53,16 +58,36 @@ void deleteTail(Node* head){
     temp->next=NULL;
 }
 Node* deletekthElement(Node* head, int k){
-    
+    if(head == NULL) { return head; }
+    if(k==1){
+        return deleteHead(head);
+    }
+    int count = 1;
+    Node* temp = head;
+    while(temp->next!=nullptr && count<k){
+        temp = temp->next;
+        count++;
+    }
+    if(temp==nullptr) { return head;}
+    if(temp->prev!=nullptr){
+        temp->prev->next = temp->next;
+    }
+    if(temp->next!=nullptr){
+        temp->next->prev = temp->prev;
+    }
+    delete temp;
+    return head;
 }
 int main() {
     // Write C++ code here
     vector<int> arr = {1, 2, 3, 4, 5};
     Node* head = convertArrayToDLL(arr);
     print(head);
-    head = deleteHead(head);
+    // head = deleteHead(head);
     cout<<endl;
-    deleteTail(head);
+    // deleteTail(head);
+    // print(head);
+    deletekthElement(head, 5);
     print(head);
 
     return 0;
